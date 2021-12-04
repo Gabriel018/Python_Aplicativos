ac3760b69aea638973ff5fd562e12c5339a42886
Gabriel018 <duke.comandos@gmail.com>
2021-12-04 20:54:30 -0300

    Conversor

---
+++ Converso_PDF.py
@@ -0,0 +1,48 @@
+
+
+from pdf2docx import parse
+from typing import Tuple
+import PySimpleGUI as py
+
+
+py.theme("DarkBlue")
+
+def pdf_convert(arq_pfd: str, arq_convert: str, pages: Tuple = None):
+    result = parse(pdf_file=arq_pfd,
+                   docx_file=arq_convert,
+                   pages=pages)
+
+    return result
+
+def comvert():
+    # Quando salvar o arquivo salvar ele com a extençao  " .docx "
+    
+             layout = [
+                   [py.Text("Conversor de PDF para DOCX",font="arial 14")],
+                  [py.Input(size=(30),key='pdf'),py.FileBrowse(font="Arial ")],
+                  [py.Input(size=(30),key='doc'),py.FileSaveAs(font="Arial ")],
+                  [py.Button("Converter",key="convert",font="Arial 12 "),] ]
+
+
+             janela = py.Window("Conversor de PDF", layout, size=(400, 300))
+
+             while True:
+
+              event,values = janela.read()
+              if event == "convert":
+                py.popup("Quando for salvar o arquivo salve com o final .docx ",font="arial 14")
+                arq_pfd = values['pdf']
+                arq_convert = values['doc']
+                py.popup_notify("Aguarde seu Arquivo sera convertido")
+                pdf_convert(arq_pfd, arq_convert)
+                py.popup("Concluido",font="arial 12")
+
+
+
+
+
+if __name__ == '__main__':
+ comvert()
+ arq_pfd = arq_pfd
+ arq_convert = arq_convert
+ pdf_convert(arq_pfd,arq_convert)
